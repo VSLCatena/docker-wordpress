0a7de0c96e1b09f57c32c18157206365a101c799
kipjr <martijn9873@hotmail.com>
2021-04-21 21:25:22 +0200

    WP CLI fully working

--- wp-cli.sh
+++ wp-cli.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-docker-compose run --rm wp-cli wp-cli "$@"
+docker-compose run --rm --user="33:33" -e HOME=/tmp wp_cli wp-cli $@
+
+#wp-cli core install --url=192.168.1.4:9001 --title="WP-CLI" --admin_user=wpcli --admin_password=wpcli --admin_email=info@wp-cli.org --path='/var/www/html/'
+#wp-cli plugin install better-wp-security redirection
