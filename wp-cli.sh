8ae9579e397f99f5cf431456815c522574e1be27
kipjr <martijn@koetsier.dev>
2021-05-07 18:54:58 +0200

    Update init install

--- wp-cli.sh
+++ wp-cli.sh
@@ -2,4 +2,4 @@
 docker-compose run --rm --user="33:33" -e HOME=/tmp wp_cli wp-cli $@
 
 #wp-cli core install --url=192.168.1.4:9001 --title="WP-CLI" --admin_user=wpcli --admin_password=wpcli --admin_email=info@wp-cli.org --path='/var/www/html/'
-#wp-cli plugin install better-wp-security redirection
+#wp-cli plugin install better-wp-security redirection cookie-notice disable-comments google-calendar-events wp-mail-smtp maintenance wordfence
