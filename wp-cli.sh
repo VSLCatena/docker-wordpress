02a558c1ea26bdf0440235a1ba20868b88fbf4aa
Martie <martijn9873@hotmail.com>
2021-07-01 16:59:29 +0300

    Rebuild code

--- wp-cli.sh
+++ wp-cli.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
-read -p "wp-cli container name:" container
-if [ -f './.WP_INITIALIZED' ]; then
-    docker-compose run --rm --user="33:33" -e HOME=/tmp $container wp-cli $@
+if [[ $1 == "init" ]]; then
+    wp_url=$2
+    wp_title=$3
+    wp_user="netcie"
+    wp_mail="netcie@vslcatena.nl"
+    wp_pw=$4
+    wp_container=$5
+    wp_docker="docker-compose run --rm --user="33:33" -e HOME=/tmp $wp_container"
+    if [ -f './.WP_INITIALIZED' ]; then
+        echo "already initialised"
+    else
+        touch ./.WP_INITIALIZED
+        $wp_docker core install --url=$wp_url --title=$wp_title --admin_user=$wp_user --admin_password=$wp_pw --admin_email=$wp_mail
+        $wp_docker plugin install --activate better-wp-security redirection cookie-notice disable-comments google-calendar-events wp-mail-smtp maintenance wordfence
+        $wp_docker plugin auto-updates enable --all
+     #   $wp_docker user create $username $mail --role=administrator
+    fi
 else
-    touch ./.WP_INITIALIZED
-    read -p "Mail address of new user:" mail
-    read -p "Username of new user:" username
-    docker-compose run --rm --user="33:33" -e HOME=/tmp $container plugin install --activate better-wp-security redirection cookie-notice disable-comments google-calendar-events wp-mail-smtp maintenance wordfence
-    #docker-compose run --rm --user="33:33" -e HOME=/tmp $container plugin auto-updates enable --all
-    docker-compose run --rm --user="33:33" -e HOME=/tmp $container user create $username $mail --role=administrator
+    $wp_docker $@
 fi
+    
+
