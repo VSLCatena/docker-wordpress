de03a3af7ac6cb07175e2703ce6696e8ff543cfb
Kipjr <12066560+Kipjr@users.noreply.github.com>
2021-07-11 13:23:13 +0200

    cleanup code, add purge&update

--- wp-cli.sh
+++ wp-cli.sh
@@ -10,21 +10,32 @@ export version=0
 export verbose=0
 export rebuilt=0
 export force=0
-
+export update=0
+export purge=0
 
 showHelp() {
 # `cat << EOF` This means that cat should stop reading when EOF is detected
 cat << EOF
-Usage: ./wp-cli.sh [-V] -i [-F]
+Usage: ./wp-cli.sh -[iPU] [-VF] 
 Install Wordpress and configure for Docker
 
--h,        --help               Display help
+Exclusive commands: purge, init, update, help, {other}
+Optional commands: force, verbose
 
--i,        --init               Rebuild container and files
+-h,        --help               Display help
 
 -F,        --force              Remove compiled files
 
 -V,        --verbose            Run script in verbose mode. Will print out each step of execution.
+
+-P,        --purge              Purge all data (needs confirm)
+
+-i,        --init               Rebuild container and files based on .env
+
+-U,        --update             Update core db and plugins
+
+If none given, "$@" is executed
+
 
 EOF
 # EOF is found above and hence cat command stops reading. This is equivalent to echo but much neater when printing out.
@@ -52,7 +63,7 @@ addVirtualHost() {
         cp ./server.virtualhost.conf "${WP_URL}.conf"
         replaceVarFile "${WP_URL}.conf" WP_URL "${WP_URL}"
         replaceVarFile "${WP_URL}.conf" WP_URL "${WP_URL}"
-        cp ${WP_URL}.conf /etc/apache2/sites-enabled/${WP_URL}
+        cp ${WP_URL}.conf /etc/apache2/sites-enabled/${WP_URL}.conf
     else
         echo "apache not installed"  
     fi
@@ -75,15 +86,15 @@ getCert() {
 }
 
 initWP() {
-        #docker ps -q -f name={container Name}
-        [  $(docker-compose ps -a | grep ${WP_DB_NAME} | wc -l ) -eq 0 ]  && docker-compose up --no-start ${WP_DB_NAME} ${WP_NAME} && docker-compose start  ${WP_DB_NAME} ${WP_NAME}
-        [[ ${WP_HTTPS} == 1 ]] && WP_PROT="https" || WP_PROT="http"
-        sleep 15
-        eval $wp_docker ${WP_CLI_NAME} wp core install --url="${WP_PROT}://${WP_URL}" --title=${WP_TITLE} --admin_user=${WP_ADMIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}
-        pluginsWP
-        sleep 15
-        #optionsWP
-     #  $wp_docker user create $username $mail --role=administrator
+    #docker ps -q -f name={container Name}
+    [  $(docker-compose ps -a | grep ${WP_DB_NAME} | wc -l ) -eq 0 ]  && docker-compose up --no-start ${WP_DB_NAME} ${WP_NAME} && docker-compose start  ${WP_DB_NAME} ${WP_NAME}
+    [[ ${WP_HTTPS} == 1 ]] && WP_PROT="https" || WP_PROT="http"
+    sleep 15
+    eval $wp_docker ${WP_CLI_NAME} wp core install --url="${WP_PROT}://${WP_URL}" --title=${WP_TITLE} --admin_user=${WP_ADMIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}
+    pluginsWP
+    sleep 15
+    #optionsWP
+    #  $wp_docker user create $username $mail --role=administrator
 }
 
 pluginsWP() {
@@ -112,16 +123,43 @@ optionsWP() {
 
 }
 
-main() {
-    if [[ -f './.WP_INITIALIZED' && $force == 0 ]]; then
-        echo "already initialised"
-        exit 0
-    elif [ $force == 1 ]; then
+cleanWP() {
         docker-compose rm --force --stop -v
         docker volume prune --force  --filter label=com.docker.compose.project=$(basename "$PWD")
         rm ./"${WP_URL}.conf"
         rm ./.WP_INITIALIZED
         rm ./docker-compose.yml
+        rm /etc/apache2/sites
+        rm /etc/apache2/sites-enabled/${WP_URL}.conf
+}
+purge() {
+    read -p "Are you sure (Y/n)" q
+    if [[ $q == "Y" ]]; then
+        cleanWP
+        echo "You need to manually remove DNS entry"
+        read -p "Delete folder (Y/n)" f
+        if [[ $f == "Y" ]]; then
+            rm ../$(basename "$PWD")
+        fi
+    else 
+        echo "Action aborted"
+        exit 0
+    fi
+}
+
+update() {
+    eval $wp_docker ${WP_CLI_NAME} wp core check-update
+    eval $wp_docker ${WP_CLI_NAME} wp core update
+    eval $wp_docker ${WP_CLI_NAME} wp core update-db
+    eval $wp_docker ${WP_CLI_NAME} wp plugin update --all
+}
+
+main() {
+    if [[ -f './.WP_INITIALIZED' && $force == 0 ]]; then
+        echo "already initialised"
+        exit 0
+    elif [ $force == 1 ]; then
+        cleanWP
         export force=0
         main
     elif [[ ! -f './.WP_INITIALIZED'  ]]; then
@@ -138,9 +176,11 @@ main() {
         echo "" 
     fi
 }
-
-
-
+#####
+#####  Execution of commands
+#####
+#
+#
 # $@ is all command line parameters passed to the script.
 # -o is for short options like -v
 # -l is for long options with double dash like --version
@@ -164,13 +204,18 @@ case $1 in
     export verbose=1
     set -xv  # Set xtrace and verbose mode.
     ;;
--i|--init)
-    export init=1
-    ;;
 -F|--force)
     export force=1
     ;;
-
+-i|--init)
+    export init=1
+    ;;
+-P|--purge)
+    export purge=1
+    ;;
+-U|--update)
+    export update=1
+    ;;    
 --)
     shift
     break
@@ -179,10 +224,23 @@ esac
 shift
 done
 
+commands=$init+$purge+$update #exclusive commands
 
+if [[ $commands == 1 ]]; then
 
-if [[ $init == 1 ]]; then
-    main
+    if [[ $init == 1 ]]; then
+        main
+    elif [[ $purge == 1 ]]; then
+        purge
+    elif [[ $update == 1 ]]; then
+        update
+    else
+    if [[  eval $wp_docker ${WP_CLI_NAME} wp core is-installed ]]; then
+        eval ${wp_docker} ${WP_CLI_NAME} "$@"
+    else 
+        echo "Unable to execute, WP not installed" 
+    fi
+
 else
-    eval ${wp_docker} ${WP_CLI_NAME} "$@"
+    echo "Too many commands given"
 fi
