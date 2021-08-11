a2f3ef1faa3b2a6c0728de31c3248157e6452298
Kipjr <12066560+Kipjr@users.noreply.github.com>
2021-08-11 23:24:41 +0200

    Fix: Code Scan SC2124 (1x)

--- wp-cli.sh
+++ wp-cli.sh
@@ -351,7 +351,7 @@ if [[ $commands == 1 ]]; then
 elif [[ $commands == 0 ]]; then
     [[ $(eval $wp_docker ${WP_CLI_NAME} wp core is-installed) == "" ]] && error=0 || error=1
     if [[ $error  == 0 ]]; then
-        [[ -z "$@" ]] && com="wp cli info" || com="$@"
+        [[ -z "$@" ]] && com="wp cli info" || com="$*"
         eval ${wp_docker} ${WP_CLI_NAME} ${com};
     else 
         echo "Unable to execute, WP not installed" | writeLog "ERR"
